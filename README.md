4556fc2759cf5aa4e479eea8afe4b0b04b62f233
dingzhengru <qws7869vdx@gmail.com>
2022-03-03 11:01:17 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cypress-first
+
+## 如何使用 node_modules
+
+- 官方說明: [連結](https://docs.cypress.io/faq/questions/using-cypress-faq#How-do-I-require-or-import-node-modules-in-Cypress)
+- cypress task: [連結](https://docs.cypress.io/api/commands/task)
+- cypress plugin: [連結](https://docs.cypress.io/api/plugins/writing-a-plugin)
+
+因為 spec.js 的代碼是在瀏覽器中執行的，所以無法直接引入使用  
+需在 plugins/index.js 中新增 task (執行 nodejs 代碼) & exec (執行 shell command)  
+來讓 spec 使用，另外有預設的 task 可使用 (參考上面 plugin 連結)
---
+++ cypress/plugins/index.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+const { createWorker } = require('tesseract.js');
+const Jimp = require('jimp');
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+
+  on('after:screenshot', details => {
+    console.log('-----------------------after:screenshot---------------------------');
+    console.log(details);
+  });
+
+  on('task', {
+    async getImageText(imgPath) {
+      console.log('--------------task: getImageText----------------');
+      const worker = createWorker();
+      await worker.load();
+      await worker.loadLanguage('eng');
+      await worker.initialize('eng');
+
+      const jimpImage = await Jimp.read(imgPath);
+      const img = await jimpImage.greyscale().contrast(1).getBase64Async(Jimp.AUTO);
+      const result = await worker.recognize(img);
+      console.log('------------getImageText------------');
+      await worker.terminate();
+      return result.data.text;
+    },
+  });
+};
